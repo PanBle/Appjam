5ab862e9ae09339e2bc66cb79650afa8ebabd7a1
PanBle <ktm0921978@gmail.com>
2018-12-22 01:47:40 +0900

    first commit

---
+++ appjam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appjam
+//
+//  Created by KM_TM on 22/12/2018.
+//  Copyright © 2018 KM_TM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
